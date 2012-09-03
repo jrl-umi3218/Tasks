d8d3dc3737f19262feb24a58663724d203c27b9b
Joris Vaillant <joris.vaillant@gmail.com>
2012-09-03 18:37:47 +0200

    First commit.

---
+++ src/Tasks.cpp
@@ -0,0 +1,294 @@
+// This file is part of Tasks.
+//
+// Tasks is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tasks is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Tasks.  If not, see <http://www.gnu.org/licenses/>.
+
+
+// associated header
+#include "Tasks.h"
+
+// includes
+// rbd
+#include <MultiBody.h>
+#include <MultiBodyConfig.h>
+
+namespace tasks
+{
+
+PositionTask::PositionTask(const rbd::MultiBody& mb, int bodyId,
+  const Eigen::Vector3d& pos):
+  pos_(pos),
+  bodyIndex_(mb.bodyIndexById(bodyId)),
+  jac_(mb, bodyId),
+  eval_(3),
+  shortJacMat_(3, jac_.jointsPath().size()),
+  jacMat_(3, mb.nrDof()),
+  jacDotMat_(3, mb.nrDof())
+{
+}
+
+
+void PositionTask::position(const Eigen::Vector3d& pos)
+{
+	pos_ = pos;
+}
+
+
+const Eigen::Vector3d& PositionTask::position() const
+{
+	return pos_;
+}
+
+
+void PositionTask::update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	eval_ = pos_ - mbc.bodyPosW[bodyIndex_].translation();
+
+	shortJacMat_ = jac_.jacobian(mb, mbc).block(3, 0, 3, shortJacMat_.cols());
+	jac_.fullJacobian(mb, shortJacMat_, jacMat_);
+}
+
+
+void PositionTask::updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	shortJacMat_ = jac_.jacobianDot(mb, mbc).block(3, 0, 3, shortJacMat_.cols());
+	jac_.fullJacobian(mb, shortJacMat_, jacDotMat_);
+}
+
+
+const Eigen::VectorXd& PositionTask::eval() const
+{
+	return eval_;
+}
+
+
+const Eigen::MatrixXd& PositionTask::jac() const
+{
+	return jacMat_;
+}
+
+
+const Eigen::MatrixXd& PositionTask::jacDot() const
+{
+	return jacDotMat_;
+}
+
+
+
+OrientationTask::OrientationTask(const rbd::MultiBody& mb, int bodyId, const Eigen::Quaterniond& ori):
+  ori_(ori.matrix()),
+  bodyIndex_(mb.bodyIndexById(bodyId)),
+  jac_(mb, bodyId),
+  eval_(3),
+  shortJacMat_(3, jac_.jointsPath().size()),
+  jacMat_(3, mb.nrDof()),
+  jacDotMat_(3, mb.nrDof())
+{
+}
+
+
+OrientationTask::OrientationTask(const rbd::MultiBody& mb, int bodyId, const Eigen::Matrix3d& ori):
+  ori_(ori),
+  bodyIndex_(mb.bodyIndexById(bodyId)),
+  jac_(mb, bodyId),
+  eval_(3),
+  shortJacMat_(3, jac_.jointsPath().size()),
+  jacMat_(3, mb.nrDof()),
+  jacDotMat_(3, mb.nrDof())
+{
+}
+
+
+void OrientationTask::orientation(const Eigen::Quaterniond& ori)
+{
+	ori_ = ori.matrix();
+}
+
+
+void OrientationTask::orientation(const Eigen::Matrix3d& ori)
+{
+	ori_ = ori;
+}
+
+
+const Eigen::Matrix3d& OrientationTask::orientation() const
+{
+	return ori_;
+}
+
+
+void OrientationTask::update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	eval_ = sva::rotationError(mbc.bodyPosW[bodyIndex_].rotation(), ori_);
+
+	shortJacMat_ = jac_.jacobian(mb, mbc).block(0, 0, 3, shortJacMat_.cols());
+	jac_.fullJacobian(mb, shortJacMat_, jacMat_);
+}
+
+
+void OrientationTask::updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	shortJacMat_ = jac_.jacobianDot(mb, mbc).block(0, 0, 3, shortJacMat_.cols());
+	jac_.fullJacobian(mb, shortJacMat_, jacDotMat_);
+}
+
+
+const Eigen::VectorXd& OrientationTask::eval() const
+{
+	return eval_;
+}
+
+
+const Eigen::MatrixXd& OrientationTask::jac() const
+{
+	return jacMat_;
+}
+
+
+const Eigen::MatrixXd& OrientationTask::jacDot() const
+{
+	return jacDotMat_;
+}
+
+
+
+PostureTask::PostureTask(const rbd::MultiBody& mb, std::vector<std::vector<double>> q):
+	q_(q),
+	eval_(mb.nrDof()),
+	jacMat_(mb.nrDof(), mb.nrDof()),
+	jacDotMat_(mb.nrDof(), mb.nrDof())
+{
+	jacMat_.setIdentity();
+	jacDotMat_.setZero();
+}
+
+
+void PostureTask::posture(std::vector<std::vector<double>> q)
+{
+	q_ = q;
+}
+
+
+const std::vector<std::vector<double>> PostureTask::posture() const
+{
+	return q_;
+}
+
+
+void PostureTask::update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	using namespace Eigen;
+
+	// we drop the first joint (fixed or free flyier).
+	for(std::size_t i = 1; i < mb.nrJoints(); ++i)
+	{
+		int pos = 0;
+
+		// if dof == 1 is a prismatic/revolute joint
+		// else if dof == 4 is a spherical one
+		// else is a fixed one
+		if(mb.joint(i).dof() == 1)
+		{
+			eval_(pos) = q_[i][0] - mbc.q[i][0];
+			++pos;
+		}
+		else if(mb.joint(i).dof() == 4)
+		{
+			Matrix3d orid(
+				Quaterniond(q_[i][0], q_[i][1], q_[i][2], q_[i][3]).matrix());
+
+			Vector3d err = sva::rotationError(mbc.jointConfig[i].rotation(), orid);
+
+			eval_.segment(pos, 3) = err;
+			pos += 3;
+		}
+	}
+}
+
+
+void PostureTask::updateDot(const rbd::MultiBody& /* mb */, const rbd::MultiBodyConfig& /* mbc */)
+{}
+
+
+const Eigen::VectorXd& PostureTask::eval() const
+{
+	return eval_;
+}
+
+
+const Eigen::MatrixXd& PostureTask::jac() const
+{
+	return jacMat_;
+}
+
+
+const Eigen::MatrixXd& PostureTask::jacDot() const
+{
+	return jacDotMat_;
+}
+
+
+
+CoMTask::CoMTask(const rbd::MultiBody& mb, Eigen::Vector3d& com):
+  com_(com),
+  jac_(mb),
+  eval_(3),
+  jacMat_(3, mb.nrDof()),
+  jacDotMat_(3, mb.nrDof())
+{}
+
+
+void CoMTask::com(const Eigen::Vector3d& com)
+{
+  com_ = com;
+}
+
+
+const Eigen::Vector3d CoMTask::com() const
+{
+  return com_;
+}
+
+
+void CoMTask::update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	eval_ = com_ - rbd::computeCoM(mb, mbc);
+
+	jacMat_ = jac_.jacobian(mb, mbc).block(3, 0, 3, mb.nrDof());
+}
+
+
+void CoMTask::updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc)
+{
+	/// @todo CoM dot jacobian
+}
+
+
+const Eigen::VectorXd& CoMTask::eval() const
+{
+	return eval_;
+}
+
+
+const Eigen::MatrixXd& CoMTask::jac() const
+{
+	return jacMat_;
+}
+
+
+const Eigen::MatrixXd& CoMTask::jacDot() const
+{
+	return jacDotMat_;
+}
+
+} // namespace tasks
---
+++ src/Tasks.h
@@ -0,0 +1,145 @@
+// This file is part of Tasks.
+//
+// Tasks is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tasks is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Tasks.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// includes
+// Eigen
+#include <Eigen/Core>
+
+// RBDyn
+#include <CoM.h>
+#include <Jacobian.h>
+
+// forward declarations
+// rbd
+namespace rbd
+{
+	class MultiBody;
+	class MultiBodyConfig;
+}
+
+namespace tasks
+{
+
+
+class PositionTask
+{
+public:
+	PositionTask(const rbd::MultiBody& mb, int bodyId, const Eigen::Vector3d& pos);
+
+	void position(const Eigen::Vector3d& pos);
+	const Eigen::Vector3d& position() const;
+
+	void update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+	void updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+
+	const Eigen::VectorXd& eval() const;
+	const Eigen::MatrixXd& jac() const;
+	const Eigen::MatrixXd& jacDot() const;
+
+private:
+	Eigen::Vector3d pos_;
+	int bodyIndex_;
+	rbd::Jacobian jac_;
+
+	Eigen::VectorXd eval_;
+	Eigen::MatrixXd shortJacMat_;
+	Eigen::MatrixXd jacMat_;
+	Eigen::MatrixXd jacDotMat_;
+};
+
+
+
+class OrientationTask
+{
+public:
+	OrientationTask(const rbd::MultiBody& mb, int bodyId, const Eigen::Quaterniond& ori);
+	OrientationTask(const rbd::MultiBody& mb, int bodyId, const Eigen::Matrix3d& ori);
+
+	void orientation(const Eigen::Quaterniond& ori);
+	void orientation(const Eigen::Matrix3d& ori);
+	const Eigen::Matrix3d& orientation() const;
+
+	void update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+	void updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+
+	const Eigen::VectorXd& eval() const;
+	const Eigen::MatrixXd& jac() const;
+	const Eigen::MatrixXd& jacDot() const;
+
+private:
+	Eigen::Matrix3d ori_;
+	int bodyIndex_;
+	rbd::Jacobian jac_;
+
+	Eigen::VectorXd eval_;
+	Eigen::MatrixXd shortJacMat_;
+	Eigen::MatrixXd jacMat_;
+	Eigen::MatrixXd jacDotMat_;
+};
+
+
+
+class PostureTask
+{
+public:
+	PostureTask(const rbd::MultiBody& mb, std::vector<std::vector<double>> q);
+
+	void posture(std::vector<std::vector<double>> q);
+	const std::vector<std::vector<double>> posture() const;
+
+	void update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+	void updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+
+	const Eigen::VectorXd& eval() const;
+	const Eigen::MatrixXd& jac() const;
+	const Eigen::MatrixXd& jacDot() const;
+
+private:
+	std::vector<std::vector<double>> q_;
+
+	Eigen::VectorXd eval_;
+	Eigen::MatrixXd jacMat_;
+	Eigen::MatrixXd jacDotMat_;
+};
+
+
+
+class CoMTask
+{
+public:
+	CoMTask(const rbd::MultiBody& mb, Eigen::Vector3d& com);
+
+	void com(const Eigen::Vector3d& com);
+	const Eigen::Vector3d com() const;
+
+	void update(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+	void updateDot(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+
+	const Eigen::VectorXd& eval() const;
+	const Eigen::MatrixXd& jac() const;
+	const Eigen::MatrixXd& jacDot() const;
+
+private:
+	Eigen::Vector3d com_;
+	rbd::CoMJacobianDummy jac_;
+
+	Eigen::VectorXd eval_;
+	Eigen::MatrixXd jacMat_;
+	Eigen::MatrixXd jacDotMat_;
+};
+
+} // namespace tasks
