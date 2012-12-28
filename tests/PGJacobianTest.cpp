3a5a8cfdfaf49aed0eff52430d547e44c202844d
Joris Vaillant <joris.vaillant@gmail.com>
2012-12-28 14:08:04 +0100

    Run TransPgJacobianTest multiple time.

--- tests/PGJacobianTest.cpp
+++ tests/PGJacobianTest.cpp
@@ -192,6 +192,7 @@ BOOST_AUTO_TEST_CASE(RotPGJacobianTest)
 
 
 
+BOOST_AUTO_TEST_CASE(TransPGJacobianTest)
 {
 	using namespace Eigen;
 	using namespace sva;
@@ -203,61 +204,65 @@ BOOST_AUTO_TEST_CASE(RotPGJacobianTest)
 	MultiBodyConfig mbcInit;
 
 	std::tie(mb, mbcInit) = makeFreeXArm();
-	mbcInit.q[0] = randomFree();
-
-	forwardKinematics(mb, mbcInit);
-	forwardVelocity(mb, mbcInit);
-
-	MultiBodyConfig mbcStd(mbcInit);
-	MultiBodyConfig mbcPg(mbcInit);
-
-	Jacobian jacStd(mb, 1);
-	pg::PGJacobian jacPg(mb, jacStd);
-
-	MatrixXd fullStd(6, mb.nrDof());
-	MatrixXd fullPg(6, mb.nrParams());
-
-	const int nrIt = 10000;
-	const double step = 0.005;
-	const Vector3d obj(100., 50., 10.);
-
-	for(int i = 0; i < nrIt; ++i)
+	for(int i = 0; i < 5; ++i)
 	{
-		Vector3d err = obj - mbcStd.bodyPosW[1].translation();
+		mbcInit.q[0] = randomFree();
+		mbcInit.q[1][0] = Matrix<double, 1, 1>::Random()(0);
 
-		const MatrixXd& jac = jacStd.jacobian(mb, mbcStd);
-		jacStd.fullJacobian(mb, jac, fullStd);
+		forwardKinematics(mb, mbcInit);
+		forwardVelocity(mb, mbcInit);
 
-		VectorXd res =
-			fullStd.block(3, 0., 3, mb.nrDof()).jacobiSvd(ComputeThinU | ComputeThinV).solve(err);
+		MultiBodyConfig mbcStd(mbcInit);
+		MultiBodyConfig mbcPg(mbcInit);
 
-		mbcStd.alpha = vectorToDof(mb, res);
+		Jacobian jacStd(mb, 1);
+		pg::PGJacobian jacPg(mb, jacStd);
 
-		eulerIntegration(mb, mbcStd, step);
+		MatrixXd fullStd(6, mb.nrDof());
+		MatrixXd fullPg(6, mb.nrParams());
 
-		forwardKinematics(mb, mbcStd);
-		forwardVelocity(mb, mbcStd);
+		const int nrIt = 5000;
+		const double step = 0.005;
+		const Vector3d obj(100., 50., 10.);
+
+		for(int i = 0; i < nrIt; ++i)
+		{
+			Vector3d err = obj - mbcStd.bodyPosW[1].translation();
+
+			const MatrixXd& jac = jacStd.jacobian(mb, mbcStd);
+			jacStd.fullJacobian(mb, jac, fullStd);
+
+			VectorXd res =
+				fullStd.block(3, 0., 3, mb.nrDof()).jacobiSvd(ComputeThinU | ComputeThinV).solve(err);
+
+			mbcStd.alpha = vectorToDof(mb, res);
+
+			eulerIntegration(mb, mbcStd, step);
+
+			forwardKinematics(mb, mbcStd);
+			forwardVelocity(mb, mbcStd);
+		}
+		BOOST_CHECK_SMALL((mbcStd.bodyPosW[1].translation() - obj).norm(), TOL);
+
+		for(int i = 0; i < nrIt; ++i)
+		{
+			Vector3d err = obj - mbcPg.bodyPosW[1].translation();
+
+			const MatrixXd& jac = jacPg.jacobian(mb, mbcPg, jacStd.jacobian(mb, mbcPg));
+			jacPg.fullJacobian(mb, jacStd, jac, fullPg);
+
+			VectorXd res =
+				fullPg.block(3, 0., 3, mb.nrParams()).jacobiSvd(ComputeThinU | ComputeThinV).solve(err);
+
+			VectorXd q = paramToVector(mb, mbcPg.q);
+			q += res*step;
+			q.head<4>().normalize();
+			mbcPg.q = vectorToParam(mb, q);
+
+			forwardKinematics(mb, mbcPg);
+			forwardVelocity(mb, mbcPg);
+		}
+		BOOST_CHECK_SMALL((mbcPg.bodyPosW[1].translation() - obj).norm(), TOL);
 	}
-	BOOST_CHECK_SMALL((mbcStd.bodyPosW[1].translation() - obj).norm(), TOL);
-
-	for(int i = 0; i < nrIt; ++i)
-	{
-		Vector3d err = obj - mbcPg.bodyPosW[1].translation();
-
-		const MatrixXd& jac = jacPg.jacobian(mb, mbcPg, jacStd.jacobian(mb, mbcPg));
-		jacPg.fullJacobian(mb, jacStd, jac, fullPg);
-
-		VectorXd res =
-			fullPg.block(3, 0., 3, mb.nrParams()).jacobiSvd(ComputeThinU | ComputeThinV).solve(err);
-
-		VectorXd q = paramToVector(mb, mbcPg.q);
-		q += res*step;
-		q.head<4>().normalize();
-		mbcPg.q = vectorToParam(mb, q);
-
-		forwardKinematics(mb, mbcPg);
-		forwardVelocity(mb, mbcPg);
-	}
-	BOOST_CHECK_SMALL((mbcPg.bodyPosW[1].translation() - obj).norm(), TOL);
 }
 
