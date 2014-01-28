c730c20988bde7a6cfa97e8ed314f345030c2894
Hervé Audren <h.audren@gmail.com>
2014-01-28 18:08:57 +0900

    Added small test for momentumTask

--- tests/QPSolverTest.cpp
+++ tests/QPSolverTest.cpp
@@ -1446,3 +1446,39 @@ BOOST_AUTO_TEST_CASE(QPConstantSpeedTest)
 	BOOST_CHECK_GT(std::pow(
 		compute6dError(finalPos, initPos)(4), 2), 0.1);
 }
+
+BOOST_AUTO_TEST_CASE(MomentumTask)
+{
+	using namespace Eigen;
+	using namespace sva;
+	using namespace rbd;
+	using namespace tasks;
+	namespace cst = boost::math::constants;
+
+	MultiBody mb;
+	MultiBodyConfig mbc;
+
+	std::tie(mb, mbc) = makeZXZArm();
+
+	forwardKinematics(mb, mbc);
+	forwardVelocity(mb, mbc);
+
+	qp::QPSolver solver(true);
+
+	solver.nrVars(mb, {}, {}, {}, {});
+
+	solver.updateEqConstrSize();
+	solver.updateInEqConstrSize();
+
+	sva::ForceVecd momTarget(Vector3d(1., 1., 1.), Vector3d(0., 0., 0.));
+
+	qp::MomentumTask momTask(mb, momTarget);
+	qp::SetPointTask momTaskSp(mb, &momTask, 10., 1.);
+
+	// Test addTask
+	solver.addTask(&momTaskSp);
+	BOOST_CHECK_EQUAL(solver.nrTasks(), 1);
+
+	// Test MomentumTask
+	BOOST_REQUIRE(solver.update(mb, mbc, 0));
+}
