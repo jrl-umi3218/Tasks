jrl-umi3218/Tasks

commit 4cea9aa1d097f15be6a332d7604677984ede27c6

Adapt constraints/contacts to textual id

src/QPMotionConstr.h
@@ -111,12 +111,13 @@ protected:
 	{
 		ContactData() {}
 		ContactData(const rbd::MultiBody& mb,
-			int bodyId, int lambdaBegin,
+			const std::string& bodyName, int lambdaBegin,
 			std::vector<Eigen::Vector3d> points,
 			const std::vector<FrictionCone>& cones);
 
 
-		int bodyIndex, lambdaBegin;
+		std::string bodyIndex;
+		int lambdaBegin;
 		rbd::Jacobian jac;
 		std::vector<Eigen::Vector3d> points;
 		// BEWARE generator are minus to avoid one multiplication by -1 in the
@@ -165,11 +166,11 @@ protected:
 struct SpringJoint
 {
 	SpringJoint(){}
-	SpringJoint(int jId, double K, double C, double O):
-		jointId(jId),K(K),C(C),O(O)
+	SpringJoint(const std::string& jName, double K, double C, double O):
+		jointName(jName),K(K),C(C),O(O)
 	{}
 
-	int jointId;
+	std::string jointName;
 	double K, C, O;
 };
 

src/QPConstr.cpp
@@ -321,13 +321,13 @@ sch::Matrix4x4 tosch(const sva::PTransformd& t)
 
 
 CollisionConstr::BodyCollData::BodyCollData(const rbd::MultiBody& mb,
-	int rI, int bId, sch::S_Object* h, const sva::PTransformd& X):
+	int rI, const std::string& bName, sch::S_Object* h, const sva::PTransformd& X):
 	hull(h),
-	jac(mb, bId),
+	jac(mb, bName),
 	X_op_o(X),
 	rIndex(rI),
-	bIndex(mb.bodyIndexById(bId)),
-	bodyId(bId)
+	bIndex(mb.bodyIndexByName(bName)),
+	bodyName(bName)
 {}
 
 
@@ -365,9 +365,9 @@ CollisionConstr::CollisionConstr(const std::vector<rbd::MultiBody>& mbs, double 
 
 
 void CollisionConstr::addCollision(const std::vector<rbd::MultiBody>& mbs, int collId,
-	int r1Index, int r1BodyId,
+	int r1Index, const std::string& r1BodyName,
 	sch::S_Object* body1, const sva::PTransformd& X_op1_o1,
-	int r2Index, int r2BodyId,
+	int r2Index, const std::string& r2BodyName,
 	sch::S_Object* body2, const sva::PTransformd& X_op2_o2,
 	double di, double ds, double damping, double dampingOff)
 {
@@ -376,11 +376,11 @@ void CollisionConstr::addCollision(const std::vector<rbd::MultiBody>& mbs, int c
 	std::vector<BodyCollData> bodies;
 	if(mb1.nrDof() > 0)
 	{
-		bodies.emplace_back(mb1, r1Index, r1BodyId, body1, X_op1_o1);
+		bodies.emplace_back(mb1, r1Index, r1BodyName, body1, X_op1_o1);
 	}
 	if(mb2.nrDof() > 0)
 	{
-		bodies.emplace_back(mb2, r2Index, r2BodyId, body2, X_op2_o2);
+		bodies.emplace_back(mb2, r2Index, r2BodyName, body2, X_op2_o2);
 	}
 
 	dataVec_.emplace_back(std::move(bodies), collId, body1, body2,
@@ -971,19 +971,14 @@ std::string GripperTorqueConstr::nameInEq() const
 }
 
 
-std::string GripperTorqueConstr::descInEq(const std::vector<rbd::MultiBody>& mbs,
+std::string GripperTorqueConstr::descInEq(const std::vector<rbd::MultiBody>& /* mbs */,
 	int line)
 {
 	std::stringstream ss;
 	const GripperData& gd = dataVec_[line];
 
-	const rbd::MultiBody& mb1 = mbs[gd.contactId.r1Index];
-	const rbd::MultiBody& mb2 = mbs[gd.contactId.r1Index];
-	int r1BodyIndex = mb1.bodyIndexById(gd.contactId.r1BodyId);
-	int r2BodyIndex = mb2.bodyIndexById(gd.contactId.r2BodyId);
-
-	ss << mb1.body(r1BodyIndex).name() << "/" <<
-				mb2.body(r2BodyIndex).name() << std::endl;
+	ss << gd.contactId.r1BodyName << "/" <<
+				gd.contactId.r2BodyName << std::endl;
 	ss << "limits: " << gd.torqueLimit << std::endl;
 	return ss.str();
 }
@@ -1026,30 +1021,30 @@ BoundedSpeedConstr::BoundedSpeedConstr(const std::vector<rbd::MultiBody>& mbs,
 
 
 void BoundedSpeedConstr::addBoundedSpeed(
-	const std::vector<rbd::MultiBody>& mbs, int bodyId,
+	const std::vector<rbd::MultiBody>& mbs, const std::string& bodyName,
 	const Eigen::Vector3d& bodyPoint, const Eigen::MatrixXd& dof,
 	const Eigen::VectorXd& speed)
 {
-	addBoundedSpeed(mbs, bodyId, bodyPoint, dof, speed, speed);
+	addBoundedSpeed(mbs, bodyName, bodyPoint, dof, speed, speed);
 }
 
 
 void BoundedSpeedConstr::addBoundedSpeed(
-	const std::vector<rbd::MultiBody>& mbs, int bodyId,
+	const std::vector<rbd::MultiBody>& mbs, const std::string& bodyName,
 	const Eigen::Vector3d& bodyPoint, const Eigen::MatrixXd& dof,
 	const Eigen::VectorXd& lowerSpeed, const Eigen::VectorXd& upperSpeed)
 {
-	rbd::Jacobian jac(mbs[robotIndex_], bodyId, bodyPoint);
-	cont_.push_back({jac, dof, lowerSpeed, upperSpeed, bodyId});
+	rbd::Jacobian jac(mbs[robotIndex_], bodyName, bodyPoint);
+	cont_.push_back({jac, dof, lowerSpeed, upperSpeed, bodyName});
 }
 
 
-bool BoundedSpeedConstr::removeBoundedSpeed(int bodyId)
+bool BoundedSpeedConstr::removeBoundedSpeed(const std::string& bodyName)
 {
 	auto it = std::find_if(cont_.begin(), cont_.end(),
-		[bodyId](const BoundedSpeedData& data)
+		[bodyName](const BoundedSpeedData& data)
 		{
-			return data.bodyId == bodyId;
+			return data.bodyName == bodyName;
 		});
 
 	if(it != cont_.end())

src/QPConstr.h
@@ -250,9 +250,9 @@ public:
 		* @param dampingOff \f$ \xi_{\text{off}} \f$.
 		*/
 	void addCollision(const std::vector<rbd::MultiBody>& mbs, int collId,
-		int r1Index, int r1BodyId,
+		int r1Index, const std::string& r1BodyName,
 		sch::S_Object* body1, const sva::PTransformd& X_op1_o1,
-		int r2Index, int r2BodyId,
+		int r2Index, const std::string& r2BodyName,
 		sch::S_Object* body2, const sva::PTransformd& X_op2_o2,
 		double di, double ds, double damping, double dampingOff=0.);
 
@@ -295,13 +295,14 @@ private:
 	struct BodyCollData
 	{
 		BodyCollData(const rbd::MultiBody& mb,
-			int rIndex, int bodyId, sch::S_Object* hull,
-			const sva::PTransformd& X_op_o);
+			int rIndex, const std::string& bodyName,
+			sch::S_Object* hull, const sva::PTransformd& X_op_o);
 
 		sch::S_Object* hull;
 		rbd::Jacobian jac;
 		sva::PTransformd X_op_o;
-		int rIndex, bIndex, bodyId;
+		int rIndex, bIndex;
+		std::string bodyName;
 	};
 
 	struct CollData
@@ -547,7 +548,8 @@ public:
 		* \f$ \underline{v} \in \mathbb{R}^{n} \f$ and
 		* \f$ \overline{v} \in \mathbb{R}^{n} \f$.
 		*/
-	void addBoundedSpeed(const std::vector<rbd::MultiBody>& mbs, int bodyId,
+	void addBoundedSpeed(const std::vector<rbd::MultiBody>& mbs, 
+		const std::string& bodyName,
 		const Eigen::Vector3d& bodyPoint, const Eigen::MatrixXd& dof,
 		const Eigen::VectorXd& speed);
 
@@ -564,17 +566,18 @@ public:
 		* @param lowerSpeed Lower velocity \f$ \underline{v} \in \mathbb{R}^{n} \f$.
 		* @param upperSpeed Upper velocity \f$ \overline{v} \in \mathbb{R}^{n} \f$.
 		*/
-	void addBoundedSpeed(const std::vector<rbd::MultiBody>& mbs, int bodyId,
+	void addBoundedSpeed(const std::vector<rbd::MultiBody>& mbs,
+		const std::string& bodyName,
 		const Eigen::Vector3d& bodyPoint, const Eigen::MatrixXd& dof,
 		const Eigen::VectorXd& lowerSpeed, const Eigen::VectorXd& upperSpeed);
 
 	/**
 		* Remove a bounded speed constraint.
-		* @param bodyId Remove the constraint apply on bodyId.
+		* @param bodyName Remove the constraint applied on bodyName.
 		* @return true if the constraint has been removed false if bodyId
 		* was associated with no constraint.
 		*/
-	bool removeBoundedSpeed(int bodyId);
+	bool removeBoundedSpeed(const std::string& bodyName);
 
 	/// Remove all bounded speed constraint.
 	void resetBoundedSpeeds();
@@ -607,14 +610,15 @@ private:
 	struct BoundedSpeedData
 	{
 		BoundedSpeedData(rbd::Jacobian j, const Eigen::MatrixXd& d,
-			const Eigen::VectorXd& ls, const Eigen::VectorXd& us, int bId):
+			const Eigen::VectorXd& ls, const Eigen::VectorXd& us,
+			const std::string& bName):
 			jac(j),
 			bodyPoint(j.point()),
 			dof(d),
 			lSpeed(ls),
 			uSpeed(us),
 			body(j.jointsPath().back()),
-			bodyId(bId)
+			bodyName(bName)
 		{}
 
 		rbd::Jacobian jac;
@@ -622,7 +626,7 @@ private:
 		Eigen::MatrixXd dof;
 		Eigen::VectorXd lSpeed, uSpeed;
 		int body;
-		int bodyId;
+		std::string bodyName;
 	};
 
 private:

src/QPContactConstr.cpp
@@ -166,20 +166,21 @@ void ContactConstr::updateNrVars(const std::vector<rbd::MultiBody>& mbs,
 			dof = it->second;
 		}
 		std::vector<ContactSideData> contacts;
-		auto addContact = [&mbs, &data, &contacts](int rIndex, int bId,
+		auto addContact = [&mbs, &data, &contacts](int rIndex,
+			const std::string& bName,
 			double sign, const sva::PTransformd& point)
 		{
 			if(mbs[rIndex].nrDof() > 0)
 			{
 				contacts.emplace_back(rIndex, data.alphaDBegin(rIndex), sign,
-															rbd::Jacobian(mbs[rIndex], bId), point);
+							rbd::Jacobian(mbs[rIndex], bName), point);
 			}
 		};
-		addContact(cC.cId.r1Index, cC.cId.r1BodyId, 1., cC.X_b1_cf);
-		addContact(cC.cId.r2Index, cC.cId.r2BodyId, -1., cC.X_b1_cf*cC.X_b1_b2.inv());
+		addContact(cC.cId.r1Index, cC.cId.r1BodyName, 1., cC.X_b1_cf);
+		addContact(cC.cId.r2Index, cC.cId.r2BodyName, -1., cC.X_b1_cf*cC.X_b1_b2.inv());
 
-		int b1Index = mbs[cC.cId.r1Index].bodyIndexById(cC.cId.r1BodyId);
-		int b2Index = mbs[cC.cId.r2Index].bodyIndexById(cC.cId.r2BodyId);
+		int b1Index = mbs[cC.cId.r1Index].bodyIndexByName(cC.cId.r1BodyName);
+		int b2Index = mbs[cC.cId.r2Index].bodyIndexByName(cC.cId.r2BodyName);
 		cont_.emplace_back(std::move(contacts), dof, b1Index, b2Index, cC.X_b1_b2,
 			cC.X_b1_cf, cC.cId);
 	}

src/QPContacts.cpp
@@ -84,17 +84,18 @@ FrictionCone::FrictionCone(const Eigen::Matrix3d& frame, int nrGen, double mu,
 ContactId::ContactId():
 	r1Index(-1),
 	r2Index(-1),
-	r1BodyId(-1),
-	r2BodyId(-1),
+	r1BodyName(""),
+	r2BodyName(""),
 	ambiguityId(-1)
 {}
 
 
-ContactId::ContactId(int r1I, int r2I, int r1BId, int r2BId, int ambId):
+ContactId::ContactId(int r1I, int r2I,
+	const std::string& r1BName, const std::string& r2BName, int ambId):
 	r1Index(r1I),
 	r2Index(r2I),
-	r1BodyId(r1BId),
-	r2BodyId(r2BId),
+	r1BodyName(r1BName),
+	r2BodyName(r2BName),
 	ambiguityId(ambId)
 {}
 
@@ -102,7 +103,7 @@ ContactId::ContactId(int r1I, int r2I, int r1BId, int r2BId, int ambId):
 bool ContactId::operator==(const ContactId& cId) const
 {
 	return r1Index == cId.r1Index && r2Index == cId.r2Index &&
-	r1BodyId == cId.r1BodyId && r2BodyId == cId.r2BodyId &&
+	r1BodyName == cId.r1BodyName && r2BodyName == cId.r2BodyName &&
 	ambiguityId == cId.ambiguityId;
 }
 
@@ -116,13 +117,13 @@ bool ContactId::operator!=(const ContactId& cId) const
 bool ContactId::operator<(const ContactId& cId) const
 {
 	return r1Index < cId.r1Index ||
-		(r1Index == cId.r1Index && r1BodyId < cId.r1BodyId) ||
-		(r1Index == cId.r1Index && r1BodyId == cId.r1BodyId &&
+		(r1Index == cId.r1Index && r1BodyName < cId.r1BodyName) ||
+		(r1Index == cId.r1Index && r1BodyName == cId.r1BodyName &&
 		r2Index < cId.r2Index) ||
-		(r1Index == cId.r1Index && r1BodyId == cId.r1BodyId &&
-		r2Index == cId.r2Index && r2BodyId < cId.r2BodyId) ||
-		(r1Index == cId.r1Index && r1BodyId == cId.r1BodyId &&
-		r2Index == cId.r2Index && r2BodyId == cId.r2BodyId &&
+		(r1Index == cId.r1Index && r1BodyName == cId.r1BodyName &&
+		r2Index == cId.r2Index && r2BodyName < cId.r2BodyName) ||
+		(r1Index == cId.r1Index && r1BodyName == cId.r1BodyName &&
+		r2Index == cId.r2Index && r2BodyName == cId.r2BodyName &&
 		ambiguityId < cId.ambiguityId);
 }
 
@@ -135,13 +136,13 @@ bool ContactId::operator<(const ContactId& cId) const
 
 
 UnilateralContact::UnilateralContact(int r1I, int r2I,
-	int r1BId, int r2BId,
+	const std::string& r1BName, const std::string& r2BName,
 	std::vector<Eigen::Vector3d> r1P,
 	const Eigen::Matrix3d& r1Frame,
 	const sva::PTransformd& Xbb,
 	int nrGen, double mu,
 	const sva::PTransformd& Xbcf):
-	contactId(r1I, r2I, r1BId, r2BId),
+	contactId(r1I, r2I, r1BName, r2BName),
 	r1Points(std::move(r1P)),
 	r2Points(),
 	r1Cone(r1Frame, nrGen, mu),
@@ -154,13 +155,13 @@ UnilateralContact::UnilateralContact(int r1I, int r2I,
 
 
 UnilateralContact::UnilateralContact(int r1I, int r2I,
-	int r1BId, int r2BId, int ambId,
+	const std::string& r1BName, const std::string& r2BName, int ambId,
 	std::vector<Eigen::Vector3d> r1P,
 	const Eigen::Matrix3d& r1Frame,
 	const sva::PTransformd& Xbb,
 	int nrGen, double mu,
 	const sva::PTransformd& Xbcf):
-	contactId(r1I, r2I, r1BId, r2BId, ambId),
+	contactId(r1I, r2I, r1BName, r2BName, ambId),
 	r1Points(std::move(r1P)),
 	r2Points(),
 	r1Cone(r1Frame, nrGen, mu),
@@ -343,13 +344,13 @@ void UnilateralContact::construct(const Eigen::MatrixXd& r1Frame, int nrGen, dou
 
 
 BilateralContact::BilateralContact(int r1I, int r2I,
-	int r1BId, int r2BId,
+	const std::string& r1BName, const std::string& r2BName,
 	std::vector<Eigen::Vector3d> r1P,
 	const std::vector<Eigen::Matrix3d>& r1Frames,
 	const sva::PTransformd& Xbb,
 	int nrGen, double mu,
 	const sva::PTransformd& Xbcf):
-	contactId(r1I, r2I, r1BId, r2BId),
+	contactId(r1I, r2I, r1BName, r2BName),
 	r1Points(std::move(r1P)),
 	r2Points(),
 	r1Cones(r1Points.size()),
@@ -362,13 +363,14 @@ BilateralContact::BilateralContact(int r1I, int r2I,
 
 
 BilateralContact::BilateralContact(int r1I, int r2I,
-	int r1BId, int r2BId, int ambId,
+	const std::string& r1BName, const std::string& r2BName,
+	int ambId,
 	std::vector<Eigen::Vector3d> r1P,
 	const std::vector<Eigen::Matrix3d>& r1Frames,
 	const sva::PTransformd& Xbb,
 	int nrGen, double mu,
 	const sva::PTransformd& Xbcf):
-	contactId(r1I, r2I, r1BId, r2BId, ambId),
+	contactId(r1I, r2I, r1BName, r2BName, ambId),
 	r1Points(std::move(r1P)),
 	r2Points(),
 	r1Cones(r1Points.size()),

src/QPContacts.h
@@ -69,12 +69,13 @@ struct TASKS_DLLAPI ContactId
 	/**
 		* @param r1Index First robot imply in the contact.
 		* @param r2Index Second robot imply in the contact.
-		* @param r1BodyId Body id of robot r1Index imply in the contact.
-		* @param r2BodyId Body id of robot r2Index imply in the contact.
+		* @param r1BodyName Body name of robot r1Index imply in the contact.
+		* @param r2BodyName Body name of robot r2Index imply in the contact.
 		* @param ambiguityId If two or more contacts have the same r1Index, r2Index,
 		* r1BodyId, r2BodyId the ambiguityId is used to dissociate them.
 		*/
-	ContactId(int r1Index, int r2Index, int r1BodyId, int r2BodyId,
+	ContactId(int r1Index, int r2Index,
+		const std::string& r1BodyName, const std::string& r2BodyName,
 		int ambiguityId=-1);
 
 	bool operator==(const ContactId& cId) const;
@@ -83,7 +84,7 @@ struct TASKS_DLLAPI ContactId
 	bool operator<(const ContactId& cId) const;
 
 	int r1Index, r2Index;
-	int r1BodyId, r2BodyId;
+	std::string r1BodyName, r2BodyName;
 	int ambiguityId;
 };
 
@@ -100,8 +101,8 @@ struct TASKS_DLLAPI UnilateralContact
 	/**
 		* @param r1Index First robot imply in the contact.
 		* @param r2Index Second robot imply in the contact.
-		* @param r1BodyId Body id of robot r1Index imply in the contact.
-		* @param r2BodyId Body id of robot r2Index imply in the contact.
+		* @param r1BodyName Body name of robot r1Index imply in the contact.
+		* @param r2BodyName Body name of robot r2Index imply in the contact.
 		* @param r1Points Contact points in r1BodyId frame.
 		* @param r1Frame Friction cone frame in rbBodyId frame.
 		* @param X_b1_b2 Transformation between r1BodyId and r2BodyId frame.
@@ -111,7 +112,8 @@ struct TASKS_DLLAPI UnilateralContact
 		* the contact constraints.
 		* @see ContactConstrCommon::addDofContact
 		*/
-	UnilateralContact(int r1Index, int r2Index, int r1BodyId, int r2BodyId,
+	UnilateralContact(int r1Index, int r2Index,
+		const std::string& r1BodyName, const std::string& r2BodyName,
 		std::vector<Eigen::Vector3d> r1Points,
 		const Eigen::Matrix3d& r1Frame,
 		const sva::PTransformd& X_b1_b2,
@@ -121,8 +123,8 @@ struct TASKS_DLLAPI UnilateralContact
 	/**
 		* @param r1Index First robot imply in the contact.
 		* @param r2Index Second robot imply in the contact.
-		* @param r1BodyId Body id of robot r1Index imply in the contact.
-		* @param r2BodyId Body id of robot r2Index imply in the contact.
+		* @param r1BodyName Body name of robot r1Index imply in the contact.
+		* @param r2BodyName Body name of robot r2Index imply in the contact.
 		* @param ambId ambiguityId.
 		* @param r1Points Contact points in r1BodyId frame.
 		* @param r1Frame Friction cone frame in rbBodyId frame.
@@ -134,7 +136,8 @@ struct TASKS_DLLAPI UnilateralContact
 		* @see ContactConstrCommon::addDofContact
 		* @see ContactId::ContactId
 		*/
-	UnilateralContact(int r1Index, int r2Index, int r1BodyId, int r2BodyId,
+	UnilateralContact(int r1Index, int r2Index,
+		const std::string& r1BodyName, const std::string& r2BodyName,
 		int ambId, std::vector<Eigen::Vector3d> r1Points,
 		const Eigen::Matrix3d& r1Frame,
 		const sva::PTransformd& X_b1_b2,
@@ -233,8 +236,8 @@ struct TASKS_DLLAPI BilateralContact
 	/**
 		* @param r1Index First robot imply in the contact.
 		* @param r2Index Second robot imply in the contact.
-		* @param r1BodyId Body id of robot r1Index imply in the contact.
-		* @param r2BodyId Body id of robot r2Index imply in the contact.
+		* @param r1BodyName Body name of robot r1Index imply in the contact.
+		* @param r2BodyName Body name of robot r2Index imply in the contact.
 		* @param r1Points Contact points in r1BodyId frame.
 		* @param r1Frames Friction cone frame in r1Points order in rbBodyId frame.
 		* @param X_b1_b2 Transformation between r1BodyId and r2BodyId frame.
@@ -245,7 +248,7 @@ struct TASKS_DLLAPI BilateralContact
 		* @see ContactConstrCommon::addDofContact
 		*/
 	BilateralContact(int r1Index, int r2Index,
-		int r1BodyId, int r2BodyId,
+		const std::string& r1BodyName, const std::string& r2BodyName,
 		std::vector<Eigen::Vector3d> r1Points,
 		const std::vector<Eigen::Matrix3d>& r1Frames,
 		const sva::PTransformd& X_b1_b2,
@@ -255,8 +258,8 @@ struct TASKS_DLLAPI BilateralContact
 	/**
 		* @param r1Index First robot imply in the contact.
 		* @param r2Index Second robot imply in the contact.
-		* @param r1BodyId Body id of robot r1Index imply in the contact.
-		* @param r2BodyId Body id of robot r2Index imply in the contact.
+		* @param r1BodyName Body id of robot r1Index imply in the contact.
+		* @param r2BodyName Body id of robot r2Index imply in the contact.
 		* @param ambId ambiguityId.
 		* @param r1Points Contact points in r1BodyId frame.
 		* @param r1Frames Friction cone frame in r1Points order in rbBodyId frame.
@@ -269,8 +272,8 @@ struct TASKS_DLLAPI BilateralContact
 		* @see ContactId::ContactId
 		*/
 	BilateralContact(int r1Index, int r2Index,
-		int r1BodyId, int r2BodyId, int ambId,
-		std::vector<Eigen::Vector3d> r1Points,
+		const std::string& r1BodyName, const std::string& r2BodyName,
+		int ambId, std::vector<Eigen::Vector3d> r1Points,
 		const std::vector<Eigen::Matrix3d>& r1Frames,
 		const sva::PTransformd& X_b1_b2,
 		int nrGen, double mu,

src/QPMotionConstr.cpp
@@ -62,8 +62,8 @@ void PositiveLambda::updateNrVars(const std::vector<rbd::MultiBody>& /* mbs */,
 	for(std::size_t i = 0; i < allC.size(); ++i)
 	{
 		cont_.push_back({allC[i].contactId,
-										data.lambdaBegin(int(i)),
-										allC[i].nrLambda()});
+				data.lambdaBegin(int(i)),
+				allC[i].nrLambda()});
 	}
 }
 
@@ -80,7 +80,7 @@ std::string PositiveLambda::nameBound() const
 }
 
 
-std::string PositiveLambda::descBound(const std::vector<rbd::MultiBody>& mbs,
+std::string PositiveLambda::descBound(const std::vector<rbd::MultiBody>& /* mbs */,
 	int line)
 {
 	std::ostringstream oss;
@@ -91,10 +91,8 @@ std::string PositiveLambda::descBound(const std::vector<rbd::MultiBody>& mbs,
 		int end = begin + cd.nrLambda;
 		if(line >= begin && line < end)
 		{
-			const rbd::MultiBody& mb1 = mbs[cd.cId.r1Index];
-			const rbd::MultiBody& mb2 = mbs[cd.cId.r2Index];
-			oss << "Body 1: " << mb1.body(mb1.bodyIndexById(cd.cId.r1BodyId)).name() << std::endl;
-			oss << "Body 2: " << mb2.body(mb2.bodyIndexById(cd.cId.r2BodyId)).name() << std::endl;
+			oss << "Body 1: " << cd.cId.r1BodyName << std::endl;
+			oss << "Body 2: " << cd.cId.r2BodyName << std::endl;
 			break;
 		}
 	}
@@ -127,12 +125,12 @@ const Eigen::VectorXd& PositiveLambda::Upper() const
 
 
 MotionConstrCommon::ContactData::ContactData(const rbd::MultiBody& mb,
-	int bId, int lB,
+	const std::string& bName, int lB,
 	std::vector<Eigen::Vector3d> pts,
 	const std::vector<FrictionCone>& cones):
 	bodyIndex(),
 	lambdaBegin(lB),
-	jac(mb, bId),
+	jac(mb, bName),
 	points(std::move(pts)),
 	minusGenerators(cones.size())
 {
@@ -215,13 +213,13 @@ void MotionConstrCommon::updateNrVars(const std::vector<rbd::MultiBody>& mbs,
 		const BilateralContact& c = cCont[i];
 		if(robotIndex_ == c.contactId.r1Index)
 		{
-			cont_.emplace_back(mb, c.contactId.r1BodyId, data.lambdaBegin(int(i)),
+			cont_.emplace_back(mb, c.contactId.r1BodyName, data.lambdaBegin(int(i)),
 				c.r1Points, c.r1Cones);
 		}
 		// we don't use else to manage self contact on the robot
 		if(robotIndex_ == c.contactId.r2Index)
 		{
-			cont_.emplace_back(mb, c.contactId.r2BodyId, data.lambdaBegin(int(i)),
+			cont_.emplace_back(mb, c.contactId.r2BodyName, data.lambdaBegin(int(i)),
 				c.r2Points, c.r2Cones);
 		}
 	}
@@ -372,7 +370,7 @@ MotionSpringConstr::MotionSpringConstr(
 	const rbd::MultiBody& mb = mbs[robotIndex_];
 	for(const SpringJoint& sj: springs)
 	{
-		int index = mb.jointIndexById(sj.jointId);
+		int index = mb.jointIndexByName(sj.jointName);
 		int posInDof = mb.jointPosInDof(index);
 		springs_.push_back({index, posInDof, sj.K, sj.C, sj.O});
 	}

commit 4599f224a3576a2acd2cfb9ef3b08fccdce8244c

Correct a bug introduced by 4cea9aa An index was incorrectly made a std::string.
This did not seem to produce any bugs, as bodyIndex was never used
in QPMotionConstr.

src/QPMotionConstr.h
@@ -116,7 +116,7 @@ protected:
 			const std::vector<FrictionCone>& cones);
 
 
-		std::string bodyIndex;
+		int bodyIndex;
 		int lambdaBegin;
 		rbd::Jacobian jac;
 		std::vector<Eigen::Vector3d> points;