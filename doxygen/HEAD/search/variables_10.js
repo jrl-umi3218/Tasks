d5b715266bc6abe1e80b46e5a4471b476b13a2e7
JRL/IDH Continuous Integration Tool <jrl-idh+ci@gmail.com>
2020-07-25 12:55:43 +0000

    Update HEAD Doxygen documentation. Source commit id: dc3b0c4a930b1c57a4161c05b85aa0c4f224953a

--- doxygen/HEAD/search/variables_10.js
+++ doxygen/HEAD/search/variables_10.js
@@ -3,14 +3,15 @@ var searchData=
   ['r1bodyname',['r1BodyName',['../structtasks_1_1qp_1_1ContactId.html#a47acc85ec19a848ec185d379dbd7c9ea',1,'tasks::qp::ContactId']]],
   ['r1cone',['r1Cone',['../structtasks_1_1qp_1_1UnilateralContact.html#a3e166b10c87bccb10ff5b22559171623',1,'tasks::qp::UnilateralContact']]],
   ['r1cones',['r1Cones',['../structtasks_1_1qp_1_1BilateralContact.html#a0820756bc6d3dfec5acba3d29bb09ab3',1,'tasks::qp::BilateralContact']]],
-  ['r1index',['r1Index',['../structtasks_1_1qp_1_1ContactId.html#aa0f853c4f1786b8a23a0252dcbf2678a',1,'tasks::qp::ContactId']]],
+  ['r1index',['r1Index',['../structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a36664bc33d3df6f48bff1cca3172188b',1,'tasks::qp::ContactConstr::ContactData::r1Index()'],['../structtasks_1_1qp_1_1ContactId.html#aa0f853c4f1786b8a23a0252dcbf2678a',1,'tasks::qp::ContactId::r1Index()']]],
   ['r1points',['r1Points',['../structtasks_1_1qp_1_1UnilateralContact.html#af7e3cbf6139f5f5b34dc5cc36e7ee9c5',1,'tasks::qp::UnilateralContact::r1Points()'],['../structtasks_1_1qp_1_1BilateralContact.html#aaa53327f1e4010109daaca0472cdfa87',1,'tasks::qp::BilateralContact::r1Points()']]],
   ['r2bodyname',['r2BodyName',['../structtasks_1_1qp_1_1ContactId.html#aec152e04a89289523099fd8ebe816e8c',1,'tasks::qp::ContactId']]],
   ['r2cone',['r2Cone',['../structtasks_1_1qp_1_1UnilateralContact.html#a73df9714f72365e74a0682317107b646',1,'tasks::qp::UnilateralContact']]],
   ['r2cones',['r2Cones',['../structtasks_1_1qp_1_1BilateralContact.html#a87ac911696ce421a9ba1f83b98785986',1,'tasks::qp::BilateralContact']]],
-  ['r2index',['r2Index',['../structtasks_1_1qp_1_1ContactId.html#a58515743f8024e51dc07e65a3f0447d6',1,'tasks::qp::ContactId']]],
+  ['r2index',['r2Index',['../structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a45d95d03f77de846582a64cdb1f4d888',1,'tasks::qp::ContactConstr::ContactData::r2Index()'],['../structtasks_1_1qp_1_1ContactId.html#a58515743f8024e51dc07e65a3f0447d6',1,'tasks::qp::ContactId::r2Index()']]],
   ['r2points',['r2Points',['../structtasks_1_1qp_1_1UnilateralContact.html#a3ffa099f15791cc4efddb0f2bcea16b0',1,'tasks::qp::UnilateralContact::r2Points()'],['../structtasks_1_1qp_1_1BilateralContact.html#a8e70de174d9453af6728a12bd194c163',1,'tasks::qp::BilateralContact::r2Points()']]],
   ['reducedtofull_5f',['reducedToFull_',['../classtasks_1_1qp_1_1GenQPSolver.html#a7db3d83a10b1e0c60541ff1b0b226edd',1,'tasks::qp::GenQPSolver']]],
+  ['revdof',['revDof',['../structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#ad29c82f825db591831606fc41d48b399',1,'tasks::qp::ContactConstr::ContactData']]],
   ['robotindex',['robotIndex',['../structtasks_1_1qp_1_1ContactConstr_1_1ContactSideData.html#ab8b5fe032fa924722e9e9936033ac11a',1,'tasks::qp::ContactConstr::ContactSideData']]],
   ['robotindex_5f',['robotIndex_',['../classtasks_1_1qp_1_1MotionConstrCommon.html#a3843d4962b6ddfd6daebacb7d8e8c7f8',1,'tasks::qp::MotionConstrCommon::robotIndex_()'],['../classtasks_1_1qp_1_1TransformTaskCommon.html#a55c311d3cda616f83de11d836c6f3d18',1,'tasks::qp::TransformTaskCommon::robotIndex_()']]]
 ];
--- doxygen/HEAD/structtasks_1_1qp_1_1ContactConstr_1_1ContactData.js
+++ doxygen/HEAD/structtasks_1_1qp_1_1ContactConstr_1_1ContactData.js
@@ -1,11 +1,15 @@
 var structtasks_1_1qp_1_1ContactConstr_1_1ContactData =
 [
-    [ "ContactData", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a3ab1f7d913208fd934bf01bd7cffa8c5", null ],
+    [ "ContactData", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a90801e448573ba36d10a5eb230111896", null ],
+    [ "update", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#ace2230dda7998dae13a6d67f51afc185", null ],
     [ "b1Index", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a060e74dde561d3e2b2b744c7e413b4bb", null ],
     [ "b2Index", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a5b7a1d42371e09c6453d6fc09b29a395", null ],
     [ "contactId", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a73463d49bc2a0f6e8e08b17c465278d9", null ],
     [ "contacts", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a8eaacc04f628b6ef37f84e711153da3e", null ],
     [ "dof", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a4d025b4a9ad0dc05a96b571d42fedb29", null ],
+    [ "r1Index", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a36664bc33d3df6f48bff1cca3172188b", null ],
+    [ "r2Index", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#a45d95d03f77de846582a64cdb1f4d888", null ],
+    [ "revDof", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#ad29c82f825db591831606fc41d48b399", null ],
     [ "X_b1_b2", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#afa20a55cc3a2d0ee4726aea8d0e20af4", null ],
     [ "X_b1_cf", "structtasks_1_1qp_1_1ContactConstr_1_1ContactData.html#aec6ce54631e347b5845b6bb24cc206a9", null ]
 ];
